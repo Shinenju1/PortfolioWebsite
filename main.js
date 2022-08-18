8715907ca10b0bb1f6c06ebd3928dcb94fcea71c
Shinenju1 <cosminarama97@gmail.com>
2022-08-18 14:03:12 +0200

    added responsive elements

---
+++ main.js
@@ -0,0 +1,8 @@
+// Navbar Functionality
+function toggleMenu(){
+    var menuToggle = document.querySelectorAll('.toggle');
+    var menu = document.querySelector('.menu');
+    menuToggle.classList.toggle('active')
+    menu.classList.toggle('active')
+
+}
